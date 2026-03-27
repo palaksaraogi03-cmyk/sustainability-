760425cfbf8da4dce46cc017865da44f1f92c5f0
palaksaraogi03-cmyk <palaksaraogi03@gmail.com>
2026-03-27 22:24:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
-
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def load_data():
     return pd.read_csv("sustainable_marketplace_survey_uae_2000.csv")
+
+def preprocess(df):
+    df = df.copy()
+
+    cat_cols = ['Gender', 'Income', 'Occupation', 'Preferred_Category', 'Purchase_Frequency']
+
+    for col in cat_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+
+    return df, None
